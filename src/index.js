d083613db6e836bcdce04951a5cd3760936a7d29
Jamie Martinez <martinejamie@hotmail.com>
2019-05-15 17:19:39 -0700

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-const App = function() {
-    return (
-    	<div className="ui container comments">
-	    	<ApprovalCard>
-		    	<CommentDetail author="Sam" avatar={faker.image.avatar()} />
-	    	</ApprovalCard>
-	    	<ApprovalCard>
-			    <CommentDetail author="Becky" avatar={faker.image.avatar()} />
-		    </ApprovalCard>
-		    <ApprovalCard>
-		    	<CommentDetail author="Tom" avatar={faker.image.avatar()} />
-	    	</ApprovalCard>
-    	</div>
-    );
+
+class App extends React.Component {
+	state = { 
+		lat: null,
+		errorMessage: ''
+	};
+
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message })
+		);
+	}
+
+	renderContent() {
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>Error: { this.state.errorMessage }</div>
+		};
+
+		if (!this.state.errorMessage && this.state.lat) {
+			return <SeasonDisplay lat={ this.state.lat }/>
+		};
+
+		return <Spinner message="Please accept location request" />
+	}
+
+	render() {
+		return (
+			<div className="background-red">
+				{this.renderContent()}
+			</div>
+		);
+	};
+
 };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector('#root'))
